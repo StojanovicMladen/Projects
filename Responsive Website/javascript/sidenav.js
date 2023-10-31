0a356d083fb295fd6dd610181624c1bbc2e74494
StojanovicMladen <148054291+StojanovicMladen@users.noreply.github.com>
2023-10-31 10:40:17 +0100

    Add files via upload

---
+++ Responsive Website/javascript/sidenav.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+
+  document.getElementById("sidenavDispay").style.display = "none";
+}
+  
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+
+  document.getElementById("sidenavDispay").style.display = "block";
+}
+
